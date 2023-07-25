61f27cb64f4b93668dab0ee4c5ab62cfdb792bd0
Halum88 <1234as4321@mail.ru>
2023-07-25 19:34:09 +0300

    add comment

--- app/main.py
+++ app/main.py
@@ -15,12 +15,7 @@ register_tortoise(
     add_exception_handlers=True,
     )
 
-
-@app.get("/")
-def read_root():
-    return {'Hello':'World'}
-
-
+#add tariffs in DB
 @app.post("/tariffs/")
 async def create_tariffs(data: dict):
     try:
@@ -35,11 +30,13 @@ async def create_tariffs(data: dict):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
-
+#get insurance cost
 @app.get("/tariffs/")
 async def get_tariffs(date: str, cargo_type: str, cost: float):
     try:
-        tariff = await Tariff.filter(date__lte=date, cargo_type=cargo_type).order_by('-date').first()
+        tariff = await Tariff.filter(date__lte=date, 
+                                     cargo_type=cargo_type).order_by('-date').first() #take the last one by date, because the price may not be loaded on the day of the request.
+                
         if tariff:
             calculated_cost = float(tariff.rate) * cost
             return {"calculated_cost": calculated_cost}
